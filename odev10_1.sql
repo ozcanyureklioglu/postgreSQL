46b2e383e5867975a32154f410a17ef2258848bc
Özcan Yüreklioğlu <ozcan.yu@hotmail.com>
2022-03-30 14:35:03 +0300

    Odev 10
    Odev 10 Left Right ve Full join

---
+++ odev10_1.sql
@@ -0,0 +1,2 @@
+SELECT city, country from city
+LEFT JOIN country ON city.country_id=country.country_id
---
+++ odev10_3.sql
@@ -0,0 +1,2 @@
+SELECT * from customer
+FULL JOIN rental ON customer.customer_id=rental.customer_id
